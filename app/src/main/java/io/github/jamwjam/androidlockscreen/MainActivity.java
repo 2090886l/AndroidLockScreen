a28eb5f0b30f28e8bf36019a2f262448781bcdb1
Jung Kim <jamwjam@gmail.com>
2015-06-08 13:04:08 -0400

    Implemented down-to-earth lockscreen

--- app/src/main/java/io/github/jamwjam/androidlockscreen/MainActivity.java
+++ app/src/main/java/io/github/jamwjam/androidlockscreen/MainActivity.java
@@ -1,9 +1,23 @@
 package io.github.jamwjam.androidlockscreen;
 
 import android.app.Activity;
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +25,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up lockscreen
+        makeFullScreen();
+        startService(new Intent(this, LockScreenService.class));
+
         setContentView(R.layout.activity_main);
     }
 
@@ -34,5 +53,87 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        return; //Do nothing
+    }
+
+    /**
+     * A simple method that sets the screen to fullscreen.  It removes the Notifications bar,
+     *   the Actionbar and the virtual keys (if they are on the phone)
+     */
+    public void makeFullScreen() {
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if(Build.VERSION.SDK_INT < 19) { //View.SYSTEM_UI_FLAG_IMMERSIVE is only on API 19+
+            this.getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        } else {
+            this.getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
+    }
+
+    public void unlockScreen(View view){
+        //Instead of using finish(), this totally destroys the process
+        finish();
+        //android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+    class LockScreenReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            //IF the screen was just turned on or it just booted up, start your Lock Activity
+            if(action.equals(Intent.ACTION_SCREEN_OFF) || action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+                Log.i(LockScreenReceiver.class.toString(), action.toString());
+                Intent i = new Intent(context, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            }
+        }
+    }
+
+    class LockScreenService extends Service {
+
+        BroadcastReceiver receiver;
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public void onCreate() {
+            KeyguardManager.KeyguardLock key;
+            KeyguardManager km = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+            //This is deprecated, but it is a simple way to disable the lockscreen in code
+            key = km.newKeyguardLock("IN");
+
+            key.disableKeyguard();
+
+            //Start listening for the Screen On, Screen Off, and Boot completed actions
+            IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            filter.addAction(Intent.ACTION_BOOT_COMPLETED);
+
+            //Set up a receiver to listen for the Intents in this Service
+            receiver = new LockScreenReceiver();
+            registerReceiver(receiver, filter);
+
+            super.onCreate();
+        }
+
+        @Override
+        public void onDestroy() {
+            unregisterReceiver(receiver);
+            super.onDestroy();
+        }
+
     }
 }
